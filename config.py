0eefdbe6c7de13be4508571c19f30bd6685f9fa3
Tomek B <tbarchan@gmail.com>
2026-08-14 15:51:21 +0200

    Building the integration with config.json file

--- config.py
+++ config.py
@@ -8,4 +8,23 @@ default_config: dict[str, list[str]] = {
         "Documents" : [".pdf", ".docx", ".pages"],
         "Audio" : [".mp3", ".wav"],
         "Video" : [".mp4", ".mov"],
+        "Installers" : [".exe", ".dmg"],
+        "Archives" : [".rar", ".zip"]
 }
+def load_or_create_config(config_path : Path) -> dict[str,list[str]]:
+    if not config_path.exists():
+        print(f"No config file found. Creating default config at {config_path}...")
+
+        with config_path.open("w") as file:
+            json.dump(obj=default_config, fp=file, indent=4)
+        return default_config
+    with config_path.open("r") as file:
+        data = json.load(fp=file)
+
+        if isinstance(data, dict):
+            return data
+        else:
+            print("Warning: config.json is improperly formatted. Using defaults")
+            return default_config
+
+
